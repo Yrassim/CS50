a9d41c779722a6c0c42df05da4aff539dd4813ed
Yrassim <74272046+Yrassim@users.noreply.github.com>
2021-01-28 18:49:37 -0500

    Mario pyramid in C, (Less)
    Let’s recreate that pyramid in C, albeit in text, using hashes (#) for bricks, a la the below. Each hash is a bit taller than it is wide, so the pyramid itself is also be taller than it is wide.
    
           #
          ##
         ###
        ####
       #####
      ######
     #######
    ########

---
+++ mario.c
@@ -0,0 +1,28 @@
+// less
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n < 1 || n > 8);
+    for (int i = 0; i < n; i++)
+    {
+        // draw the line for the first shape
+        for (int j = 0; j < n - i - 1; j++) // draw the line regarding to the height-number of #
+        {
+            printf(" ");
+        }
+        for (int z = 0; z < i + 1; z++)
+        {
+            printf("#");
+        }
+
+        printf("\n");  // to go to the next line
+    }
+}
