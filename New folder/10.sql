85d070feebc8c2c07447b9f9812cb7cb47b3bd4e
Yrassim <74272046+Yrassim@users.noreply.github.com>
2021-01-28 19:56:53 -0500

    Delete 10.sql

--- New folder/10.sql
+++
@@ -1,5 +0,0 @@
-SELECT DISTINCT people.name FROM people
-JOIN directors on people.id = directors.person_id
-JOIN movies on directors.movie_id = movies.id
-JOIN ratings on movies.id = ratings.movie_id
-WHERE rating >= 9.0;
