8481198c5f89ac2720766105ea9731fc12a0e5d7
Yrassim <74272046+Yrassim@users.noreply.github.com>
2021-01-28 19:57:28 -0500

    Delete 13.sql

--- New folder/13.sql
+++
@@ -1,9 +0,0 @@
-SELECT DISTINCT name FROM people
-JOIN stars on people.id = stars.person_id
-JOIN movies on stars.movie_id = movies.id
-WHERE movie_id IN
-(SELECT movie_id FROM people
-JOIN stars on people.id = stars.person_id
-JOIN movies on stars.movie_id = movies.id
-WHERE birth = 1958 AND name = "Kevin Bacon")
-AND name != "Kevin Bacon";
