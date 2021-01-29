4773f231f178f900d50c49554fce96bee2e66cb6
Yrassim <74272046+Yrassim@users.noreply.github.com>
2021-01-28 20:01:50 -0500

    Delete 12.sql

--- 12.sql
+++
@@ -1,9 +0,0 @@
-SELECT title FROM people
-JOIN stars on people.id = stars.person_id
-JOIN movies on stars.movie_id = movies.id
-WHERE name = "Johnny Depp"
-INTERSECT
-SELECT title FROM people
-JOIN stars on people.id = stars.person_id
-JOIN movies on stars.movie_id = movies.id
-WHERE name = "Helena Bonham Carter";
